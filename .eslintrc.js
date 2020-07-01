15c349f33474f46e6848604d6da0a60678d05c67
Lucas Serain <lucas.serain@outlook.com>
2020-06-30 23:17:03 -0300

    ESlint install and configuration

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    node: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-used-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+};
